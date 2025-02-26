d876e42ec45b86ec7c121f6b0c6ea315507b4d47
jaotarzan <joaovssouza59@gmail.com>
2025-02-26 16:11:28 -0300

    CHORE: adjusting the serializers view and urls, with the alterations in the modeling

--- core/portal/serializers/project.py
+++ core/portal/serializers/project.py
@@ -10,9 +10,9 @@ class ProjectListSerializer(serializers.ModelSerializer):
 class ProjectWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ['name', 'initial_date', 'final_date', 'areas', 'advisor', 'members', 'state', 'deploy_link', 'front_repo_link', 'back_repo_link', 'design_link', 'about']
+        fields = ['name', 'initial_date', 'final_date', 'areas', 'advisor', 'members', 'state', 'links', 'about']
         
 class ProjectDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ['id', 'name', 'initial_date', 'final_date', 'areas', 'advisor', 'members', 'state', 'deploy_link', 'front_repo_link', 'back_repo_link', 'design_link', 'about']
+        fields = ['id', 'name', 'initial_date', 'final_date', 'areas', 'advisor', 'members', 'state', 'links', 'about']
