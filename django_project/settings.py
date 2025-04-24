04ec19fdd46ab5cfa04fe9e6f9f083353c5c7760
jonatasperaza <jonatassilvaperaza@gmail.com>
2025-04-24 00:26:17 -0300

    FEAT: Adicionar impressão da variável SECRET_KEY no arquivo de configurações

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print(os.getenv("SECRET_KEY"))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 MODE = os.getenv("MODE", "DEVELOPMENT")
