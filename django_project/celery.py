41566b99cee5b66f7c65b75ca63c9a7b17d5d738
jonatasperaza <jonatassilvaperaza@gmail.com>
2025-04-24 22:49:41 -0300

    FEAT: Atualizar configuração do Celery para usar a variável de ambiente RABBITMQ_URL e reorganizar a inicialização

--- django_project/celery.py
+++ django_project/celery.py
@@ -1,21 +1,23 @@
 import os
 
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
-app = Celery('django_project')
+BROKER_URL = os.getenv("RABBITMQ_URL")
 
-app.conf.broker_url = os.getenv("RABBITMQ_URL")
+app = Celery('django_project', broker=BROKER_URL)
 
-app.conf.broker_connection_retry_on_startup = True
+app.conf.update(
+    broker_connection_retry_on_startup=True,
+)
+
+app.conf.task_always_eager = False
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks()
-
-app.conf.task_acks_late = True  
-app.conf.task_reject_on_worker_lost = True  
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 @app.task(bind=True)
 def debug_tasks(self):
